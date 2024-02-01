a61eb94b99a090ba44105df850be1e3861691ec4
vi_ser <vinceulisse@gmail.com>
2024-02-01 12:40:38 +0100

    first commit

---
+++ es-dadi/js/script.js
@@ -0,0 +1,5 @@
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
---
+++ es-email/js/script.js
@@ -0,0 +1,7 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare da noi un array di email casuali),
+stampa un messaggio appropriato sull’esito del controllo.
+(NON utilizzate il metodo .includes())
+*/
