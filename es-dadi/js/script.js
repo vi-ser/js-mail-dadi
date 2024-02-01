061372d04be3699a31744baea09531298236ef41
vi_ser <vinceulisse@gmail.com>
2024-02-01 13:05:44 +0100

    logica generazione punteggio dadi

--- es-dadi/js/script.js
+++ es-dadi/js/script.js
@@ -3,3 +3,32 @@ Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+// creo l'array con le facce del dado
+const dieSides = ["1", "2", "3", "4", "5", "6"];
+console.log(dieSides);
+
+// genero un numero casuale
+let randomNumber = Math.random();
+
+// creo un indice casuale tra gli elementi dell'array
+let randomIndex = Math.floor(dieSides.length * randomNumber);
+
+// scelgo l'elemento dell'array con indice generato casualmente
+let playerScore = dieSides[randomIndex];
+
+//stampa punteggio giocatore
+console.log("Ti è uscito", playerScore);
+
+// ripeto le operazioni per il computer
+randomNumber = Math.random();
+randomIndex = Math.floor(dieSides.length * randomNumber);
+cpuScore = dieSides[randomIndex];
+
+// stampa punteggio computer
+console.log("Al computer è uscito", cpuScore);
+
+
+
+
+
